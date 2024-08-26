1436ab4865014d907833f69870be8ce2939bcad6
DaniCarmo <dani93elle@hotmail.com>
2024-08-26 17:23:16 +0100

    add bootstrap

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 window.addEventListener('scroll', function() {
     const navbar = document.getElementById('navbar');
     if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
+        navbar.classList.add('navbar-dark');
+        navbar.classList.remove('navbar-light');
     } else {
-        navbar.classList.remove('scrolled');
+        navbar.classList.add('navbar-light');
+        navbar.classList.remove('navbar-dark');
     }
 });
